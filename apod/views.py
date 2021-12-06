97d3a77ba78904a46c93a79d2e0792ca9e1c1c6e
Eadwulf <eadwulfts@protonmail.com>
2021-12-06 16:54:55 -0400

    Add index view

---
+++ apod/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from .apod_data import apod_index
+
+
+def index(request):
+	template_name = 'apod/index.html'
+	context = {'apod_data': apod_index()}
+	return render(request, template_name, context)
