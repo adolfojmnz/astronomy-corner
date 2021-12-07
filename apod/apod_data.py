4aea598a7f0422539fc8be512f930d0d046c4be2
Eadwulf <eadwulfts@protonmail.com>
2021-12-07 00:43:06 -0400

    Add new methods to both save the APOD API data and work with it locally

--- apod/apod_data.py
+++ apod/apod_data.py
@@ -5,11 +5,25 @@ KEY = 'am8Cb93MAtrqcC1bUjrxcqV7Aai1OEqZHpuV1rpx'
 API = f'https://api.nasa.gov/planetary/apod?api_key={KEY}'
 
 
+def create_file_name():
+	from time import localtime
+	filepath = os.path.abspath('./apod/apod_data_json')
+	date = [f'{e}' if len(f'{e}') > 1 else f'0{e}' for e in localtime()[:3]]
+	return f'{filepath}/apod_data_{date[0]}-{date[1]}-{date[2]}.json'
+
+
+def create_json_file(data, filepath):
+	with open(filepath, 'w') as f:
+		json.dump(data, f)
+	return data
+
+
 def get_data(API, filepath=None):
 	try:
 		if filepath is None:
 			raise FileNotFoundError
 		with open(filepath) as json_file:
+			print('reached')
 			data = json.loads(json_file.read())
 			return data
 	except FileNotFoundError:
@@ -22,7 +36,14 @@ def get_data(API, filepath=None):
 
 
 def apod_index():
-	filepath = f"{os.path.abspath('./apod')}/apod.json"
+	filepath = create_file_name()
+
+	# Check wether the corresponding fie does not exist or if it is empty
+	if not os.path.isfile(filepath) or not os.stat(filepath).st_size > 0:
+		# Create the corresponding file with the APOD API data
+		return create_json_file(get_data(API), filepath)
+
+	# if the corresponding file exist, then read its data
 	return get_data(API, filepath)
 
 
