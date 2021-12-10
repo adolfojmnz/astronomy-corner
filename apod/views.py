4423718d99e24c5e8a43cdf3debd1e3b69253b68
Eadwulf <eadwulfts@protonmail.com>
2021-12-10 07:35:13 -0400

    Update index view and add methods to retrived the correct/requested data

--- apod/views.py
+++ apod/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
 
-from .apod_api.apod_data import apod_index
+from .forms import APODDate
+
+from .apod_api.main.apod_data import apod_main
+from .apod_api.main.apod_api import get_requested_date
 
 
 def index(request):
 	template_name = 'apod/index.html'
-	context = {'apod_data': apod_index()}
+	context = {
+		'apod_data': render_requested_apod(request),
+		'form': render_requested_form(request),
+	}
 	return render(request, template_name, context)
+
+
+def render_requested_form(request):
+	if request.method == 'POST':
+		form = APODDate(request.POST)
+		if form.is_valid():
+			return form
+		return APODDate()
+	if request.method == 'GET':
+		return APODDate()
+
+
+def render_requested_apod(request):
+	if request.method == 'POST':
+		date = get_requested_date(request.POST)
+	else:
+		date = str(timezone.now() - timedelta(hours=4)).split()[0]
+	print(date)
+	return apod_main(str_date=date)
