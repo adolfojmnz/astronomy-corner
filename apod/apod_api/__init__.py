0d3e9bbbbcb43d327b84dc3b3f516b57c5fc2e72
Eadwulf <eadwulfts@protonmail.com>
2021-12-10 07:23:52 -0400

    Move file to ./main

--- apod/apod_api/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['apod_api', 'apod_data', 'apod_local', 'apod_parser']
--- apod/apod_api/apod_api.py
+++
@@ -1,4 +0,0 @@
-def main_api():
-	KEY = 'am8Cb93MAtrqcC1bUjrxcqV7Aai1OEqZHpuV1rpx'
-	API = f'https://api.nasa.gov/planetary/apod?api_key={KEY}'
-	return API
