bfb671cc7abfecb56a3e7ab8c40e7f723b6ece8e
Eadwulf <eadwulfts@protonmail.com>
2021-12-27 21:38:17 -0400

    Create model Apod

--- apod/models.py
+++ apod/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class GetReversedURL:
+
+	def get_reversed_url(self, urlpattern_name, kwargs=None):
+		if kwargs: return reverse(urlpattern_name, **kwargs)
+		return reverse(urlpattern_name)
+
+
+class Apod(models.Model, GetReversedURL):
+	title = models.CharField(max_length=100)
+	date = models.DateField(
+		primary_key=True,
+		help_text='Date for an APOD',
+	)
+	json_data = models.JSONField()
+
+	def get_absolute_url(self):
+		return self.get_reversed_url('apod:apod')
+
+	def get_save_url(self):
+		return self.get_reversed_url('apod:save')
+
+	def get_delete_url(self):
+		return self.get_reversed_url('apod:delete')
+
+	def __str__(self):
+		return f'{self.date}'
