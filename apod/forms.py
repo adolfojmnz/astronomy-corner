6aa2d1a90963ed2b2296ec3a8a6afe5a88e36e34
Eadwulf <eadwulfts@protonmail.com>
2021-12-27 21:39:24 -0400

    Create ApodSaveForm and rename APODDate to ApodDateForm.

--- apod/forms.py
+++ apod/forms.py
@@ -1,9 +1,23 @@
 from django import forms
-from django.utils import timezone
+
+from .models import Apod
 
 
-class APODDate(forms.Form):
-	date = forms.DateField(label='Select A Date', widget=forms.SelectDateWidget)
+class ApodDateForm(forms.Form):
+	date = forms.DateField(
+		label='Select A Date',
+		widget=forms.SelectDateWidget,
+	)
+
+	def __str__(self):
+		return self.date
+
+
+class ApodSaveForm(forms.ModelForm):
+
+	class Meta:
+		model = Apod
+		fields = '__all__'
 
 	def __str__(self):
 		return self.date
