2a922b3297f3a9c1ccad8bb0aa3d1c223b653bf8
Eadwulf <eadwulfts@protonmail.com>
2021-12-28 23:43:09 -0400

    Add users app

--- config/settings.py
+++ config/settings.py
@@ -33,48 +33,49 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 	# Apps
 	'apod',
+	'users',
 
 	# Style Apps
 	'bootstrap5',
 
 	# Extensions
-    'django_extensions',
+	'django_extensions',
 
 	# Django Apps
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [BASE_DIR / 'templates'],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
@@ -84,10 +85,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
+	}
 }
 
 
@@ -95,18 +96,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 
@@ -138,56 +139,56 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Default Bootstrap settings
 BOOTSTRAP5 = {
 
-    # The complete URL to the Bootstrap CSS file
-    # Note that a URL can be either a string,
-    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/5.1.1/css/bootstrap.min.css",
-    # or a dict like the default value below.
-    "css_url": {
-        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
-        "integrity": "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
-        "crossorigin": "anonymous",
-    },
+	# The complete URL to the Bootstrap CSS file
+	# Note that a URL can be either a string,
+	# e.g. "https://stackpath.bootstrapcdn.com/bootstrap/5.1.1/css/bootstrap.min.css",
+	# or a dict like the default value below.
+	"css_url": {
+		"href": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+		"integrity": "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
+		"crossorigin": "anonymous",
+	},
 
-    # The complete URL to the Bootstrap JavaScript file
-    "javascript_url": {
-        "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
-        "integrity": "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T",
-        "crossorigin": "anonymous",
-    },
+	# The complete URL to the Bootstrap JavaScript file
+	"javascript_url": {
+		"url": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
+		"integrity": "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T",
+		"crossorigin": "anonymous",
+	},
 
-    # The complete URL to the Bootstrap CSS file (None means no theme)
-    "theme_url": None,
+	# The complete URL to the Bootstrap CSS file (None means no theme)
+	"theme_url": None,
 
-    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap5.html)
-    'javascript_in_head': False,
+	# Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap5.html)
+	'javascript_in_head': False,
 
-    # Label class to use in horizontal forms
-    'horizontal_label_class': 'col-md-3',
+	# Label class to use in horizontal forms
+	'horizontal_label_class': 'col-md-3',
 
-    # Field class to use in horizontal forms
-    'horizontal_field_class': 'col-md-9',
+	# Field class to use in horizontal forms
+	'horizontal_field_class': 'col-md-9',
 
-    # Set placeholder attributes to label if no placeholder is provided
-    'set_placeholder': True,
+	# Set placeholder attributes to label if no placeholder is provided
+	'set_placeholder': True,
 
-    # Class to indicate required (better to set this in your Django form)
-    'required_css_class': '',
+	# Class to indicate required (better to set this in your Django form)
+	'required_css_class': '',
 
-    # Class to indicate error (better to set this in your Django form)
-    'error_css_class': 'is-invalid',
+	# Class to indicate error (better to set this in your Django form)
+	'error_css_class': 'is-invalid',
 
-    # Class to indicate success, meaning the field has valid input (better to set this in your Django form)
-    'success_css_class': 'is-valid',
+	# Class to indicate success, meaning the field has valid input (better to set this in your Django form)
+	'success_css_class': 'is-valid',
 
-    # Renderers (only set these if you have studied the source and understand the inner workings)
-    'formset_renderers':{
-        'default': 'bootstrap5.renderers.FormsetRenderer',
-    },
-    'form_renderers': {
-        'default': 'bootstrap5.renderers.FormRenderer',
-    },
-    'field_renderers': {
-        'default': 'bootstrap5.renderers.FieldRenderer',
-        'inline': 'bootstrap5.renderers.InlineFieldRenderer',
-    },
+	# Renderers (only set these if you have studied the source and understand the inner workings)
+	'formset_renderers':{
+		'default': 'bootstrap5.renderers.FormsetRenderer',
+	},
+	'form_renderers': {
+		'default': 'bootstrap5.renderers.FormRenderer',
+	},
+	'field_renderers': {
+		'default': 'bootstrap5.renderers.FieldRenderer',
+		'inline': 'bootstrap5.renderers.InlineFieldRenderer',
+	},
 }
