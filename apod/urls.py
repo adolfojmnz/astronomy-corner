1ece7fc43805071fac9578956710f7ef0e31fea4
Eadwulf <eadwulfts@protonmail.com>
2021-12-29 22:34:24 -0400

    Create ApodGrid view

--- apod/urls.py
+++ apod/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ApodView, ApodList
+from .views import ApodView, ApodList, ApodGrid
 
 
 app_name = 'apod'
 urlpatterns = [
 	path('', ApodView.as_view(), name='apod'),
 	path('list/', ApodList.as_view(), name='list'),
+	path('grid/', ApodGrid.as_view(), name='grid'),
 ]
